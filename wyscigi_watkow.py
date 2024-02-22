ed7b3f5cb2bd6de9efac77cc95381e7d7ea9cf50
WitoldCod_console <witoldkozaruk@gmail.com>
2024-02-22 23:14:35 +0100

    próba innego programu kontroli wątków

--- wyscigi_watkow.py
+++ wyscigi_watkow.py
@@ -76,18 +76,25 @@ def thread3_Subroutine(i):
     
 def thread4_Subroutine(i):
     time.sleep(7)
-    print("Thread-3: Number of active threads:", threading.active_count())
-    print("Thread 3 Value:", i)
+    print("Thread-4: Number of active threads:", threading.active_count())
+    print("Thread 4 Value:", i)
+    
+def thread5_Subroutine(i):
+    time.sleep(9)
+    print("Thread-5: Number of active threads:", threading.active_count())
+    print("Thread 5 Value:", i)
     
 # Creating sample threads
 thread1 = threading.Thread(target=thread1_Subroutine, args=(100,), name="Thread1")
 thread2 = threading.Thread(target=thread2_Subroutine, args=(200,), name="Thread2")
 thread3 = threading.Thread(target=thread3_Subroutine, args=(300,), name="Thread3")
 thread4 = threading.Thread(target=thread4_Subroutine, args=(400,), name="Thread4")
+thread5 = threading.Thread(target=thread5_Subroutine, args=(500,), name="Thread4")
 print("START: Current active thread count: ", threading.active_count())
 # Calling start() method to initialize execution
 thread1.start()
 thread2.start()
 thread3.start()
 thread4.start()
-thread4.join() # Wait for thread-3 to join.
+thread5.start()
+thread5.join() # Wait for thread-3 to join.
