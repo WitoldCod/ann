5a67165efc3338610822e0c41f74767bf7cf220e
WitoldCod_console <witoldkozaruk@gmail.com>
2024-02-22 22:54:47 +0100

    próba innego programu

--- wyscigi_watkow.py
+++ wyscigi_watkow.py
@@ -1,55 +1,86 @@
-import threading
-import random
-import time
-from tqdm import tqdm
+# import threading
+# import random
+# import time
+# from tqdm import tqdm
 
-# Funkcja, którą będą wykonywać wątki
+# # Funkcja, którą będą wykonywać wątki
+# # def watek(id, postep):
+    # # # Losowa liczba iteracji dla wątku
+    # # ilosc_iteracji = random.randint(15, 15)
+    # # for i in range(ilosc_iteracji):
+        # # # Symulacja zadania
+        # # time.sleep(random.uniform(0.1, 0.1))
+        # # # Aktualizacja postępu wątku
+        # # postep[id] = i / ilosc_iteracji
+        # # # Wypisanie postępu wątku
+        # # print(f"Wątek {id}: {postep[id]*100:.2f}% ukończony")
+
 # def watek(id, postep):
     # # Losowa liczba iteracji dla wątku
-    # ilosc_iteracji = random.randint(15, 15)
-    # for i in range(ilosc_iteracji):
+    # ilosc_iteracji = random.randint(5, 15)
+    # for _ in tqdm(range(ilosc_iteracji), desc=f'Wątek {id}'):
         # # Symulacja zadania
-        # time.sleep(random.uniform(0.1, 0.1))
-        # # Aktualizacja postępu wątku
-        # postep[id] = i / ilosc_iteracji
-        # # Wypisanie postępu wątku
-        # print(f"Wątek {id}: {postep[id]*100:.2f}% ukończony")
-
-def watek(id, postep):
-    # Losowa liczba iteracji dla wątku
-    ilosc_iteracji = random.randint(5, 15)
-    for _ in tqdm(range(ilosc_iteracji), desc=f'Wątek {id}'):
-        # Symulacja zadania
-        time.sleep(random.uniform(0.1, 0.5))
+        # time.sleep(random.uniform(0.1, 0.5))
 
 
-def main():
-    # Pobranie liczby dostępnych wątków
-    ilosc_watkow = 1 # threading.active_count()
-    print("ilość wątków: ")
-    print(ilosc_watkow)
-    # Inicjalizacja tablicy postępu dla każdego wątku
-    # postep = [0] * ilosc_watkow
-    # Lista przechowująca wątki
-    watki = []
+# def main():
+    # # Pobranie liczby dostępnych wątków
+    # ilosc_watkow = 1 # threading.active_count()
+    # print("ilość wątków: ")
+    # print(ilosc_watkow)
+    # # Inicjalizacja tablicy postępu dla każdego wątku
+    # # postep = [0] * ilosc_watkow
+    # # Lista przechowująca wątki
+    # watki = []
 
-    # Tworzenie i uruchamianie wątków
-    for i in range(ilosc_watkow):
-        # watek_nowy = threading.Thread(target=watek, args=(i, postep))
-        watek_nowy = threading.Thread(target=watek, args=(i, None))
-        watki.append(watek_nowy)
-        watek_nowy.start()
+    # # Tworzenie i uruchamianie wątków
+    # for i in range(ilosc_watkow):
+        # # watek_nowy = threading.Thread(target=watek, args=(i, postep))
+        # watek_nowy = threading.Thread(target=watek, args=(i, None))
+        # watki.append(watek_nowy)
+        # watek_nowy.start()
 
-    # Oczekiwanie na zakończenie wszystkich wątków
-    for watek_nowy in watki:
-        print("liczba aktywnych wątków: ")
-        print(threading.active_count())
-        watek_nowy.join()
+    # # Oczekiwanie na zakończenie wszystkich wątków
+    # for watek_nowy in watki:
+        # print("liczba aktywnych wątków: ")
+        # print(threading.active_count())
+        # watek_nowy.join()
 
-    print("\n\n\n\n")
-    # print("liczba aktywnych wątków: ")
-    # print(threading.active_count())
-    print("Wszystkie wątki zakończyły pracę.")
+    # print("\n\n\n\n")
+    # # print("liczba aktywnych wątków: ")
+    # # print(threading.active_count())
+    # print("Wszystkie wątki zakończyły pracę.")
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+    # main()
+    
+    
+# Program to count active threads
+# active_count() method from Threading Module
+import threading
+import time
+# Methods for three threads..
+def thread1_Subroutine(i):
+    time.sleep(2)
+    print("Thread-1: Number of active threads:", threading.active_count())
+    print('Thread 1 Value:', i)
+
+def thread2_Subroutine(i):
+    print("Thread-2: Number of active threads:", threading.active_count())
+    print('Thread 2 Value:', i)
+    
+def thread3_Subroutine(i):
+    time.sleep(5)
+    print("Thread-3: Number of active threads:", threading.active_count())
+    print("Thread 3 Value:", i)
+    
+# Creating sample threads
+thread1 = threading.Thread(target=thread1_Subroutine, args=(100,), name="Thread1")
+thread2 = threading.Thread(target=thread2_Subroutine, args=(200,), name="Thread2")
+thread3 = threading.Thread(target=thread3_Subroutine, args=(300,), name="Thread3")
+print("START: Current active thread count: ", threading.active_count())
+# Calling start() method to initialize execution
+thread1.start()
+thread2.start()
+thread3.start()
+thread3.join() # Wait for thread-3 to join.
